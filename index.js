d643308c327c6c7388fd086cd9214a523c150668
Austin Starin <austin.starin@gmail.com>
2017-11-08 20:09:03 -0500

    Init widget

---
+++ index.js
@@ -0,0 +1,39 @@
+module.exports = {
+  extend: 'apostrophe-widgets',
+  label: 'Address',
+  addFields: [
+    {
+      name: 'lineOne',
+      label: 'Address Line 1',
+      type: 'string'
+    },
+    {
+      name: 'lineTwo',
+      label: 'Address Line 2',
+      type: 'string'
+    },
+    {
+      name: 'city',
+      label: 'City/Town',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'state',
+      label: 'State',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'zip',
+      label: 'Zip/Postal Code',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'country',
+      label: 'Country',
+      type: 'string'
+    }
+  ]
+}
